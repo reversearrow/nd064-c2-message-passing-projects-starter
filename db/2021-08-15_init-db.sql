bac0a5345ca90f07a09330fe8da72d05e9508986
Jay Shah <reversearrow@gmail.com>
2021-11-09 21:22:52 -0500

    wip

--- db/2021-08-15_init-db.sql
+++ db/2021-08-15_init-db.sql
@@ -5,7 +5,6 @@ CREATE TABLE person (
     company_name VARCHAR NOT NULL
 );
 
-
 CREATE TABLE location (
     id SERIAL PRIMARY KEY,
     person_id INT NOT NULL,
@@ -13,5 +12,6 @@ CREATE TABLE location (
     creation_time TIMESTAMP NOT NULL DEFAULT NOW(),
     FOREIGN KEY (person_id) REFERENCES person(id)
 );
+
 CREATE INDEX coordinate_idx ON location (coordinate);
 CREATE INDEX creation_time_idx ON location (creation_time);
